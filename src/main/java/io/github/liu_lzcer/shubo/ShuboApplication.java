c8e1c595dfccc0546051f470d76bb525096b983b
lzc <lzc_wed5wy@126.com>
2026-08-30 16:56:42 +0800

    refactor: 将package改成io.github.liu_lzcer.shubo

--- src/main/java/io/github/liu_lzcer/shubo/ShuboApplication.java
+++ src/main/java/io/github/liu_lzcer/shubo/ShuboApplication.java
@@ -1,4 +1,4 @@
-package com.example.shubo;
+package io.github.liu_lzcer.shubo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
