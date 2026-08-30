4f1222645e554e3a8d0fc48cd4f270b9b7f3e69c
lzc <lzc_wed5wy@126.com>
2026-08-30 16:29:19 +0800

    仓库初始化

---
+++ src/main/java/com/example/shubo/ShuboApplication.java
@@ -0,0 +1,13 @@
+package com.example.shubo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShuboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShuboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shubo/ShuboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shubo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShuboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
